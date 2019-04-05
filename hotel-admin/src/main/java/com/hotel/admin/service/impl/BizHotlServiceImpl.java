0a9b23d770133ccee50e8c8ea746eeceb7d24950
ZengCailong <47208120+ZengCailong@users.noreply.github.com>
2019-04-05 17:44:18 +0800

    默认的

--- hotel-admin/src/main/java/com/hotel/admin/service/impl/BizHotlServiceImpl.java
+++ hotel-admin/src/main/java/com/hotel/admin/service/impl/BizHotlServiceImpl.java
@@ -1,15 +1,19 @@
 package com.hotel.admin.service.impl;
 
-import java.util.List;
-
+import com.hotel.admin.model.CrtId;
 import com.hotel.admin.mapper.BizHotlMapper;
+import com.hotel.admin.mapper.CrtIdMapper;
 import com.hotel.admin.model.BizHotl;
 import com.hotel.admin.service.BizHotlService;
+import com.hotel.core.http.HttpResult;
 import com.hotel.core.page.MybatisPageHelper;
 import com.hotel.core.page.PageRequest;
 import com.hotel.core.page.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 /**
@@ -27,17 +31,59 @@ public class BizHotlServiceImpl implements BizHotlService {
 	@Autowired
 	private BizHotlMapper bizHotlMapper;
 
+	@Autowired
+	private CrtIdMapper crtIdMapper;
+
 	@Override
+	@Transactional
 	public int save(BizHotl record) {
-		if(record.getHotelCode() == null || record.getHotelCode() == "0") {
-			return bizHotlMapper.add(record);
-		}
+            if(record.getHotelCode() == null || record.getHotelCode() == "0") {
+                System.out.println("licy12345");
+                /* 获取自增序列并加1*/
+                System.out.println(record.getHotelCode());
+				 CrtId crt =crtIdMapper.findById("hotel");
+				 if(crt == null)
+				 {
+					 CrtId ncrt = new CrtId();
+					 System.out.println("licy1");
+					 ncrt.setCrtNo("1");
+					 ncrt.setCrtType("hotel");
+					 crtIdMapper.add(ncrt);
+					 record.setHotelCode("1");
+				 }
+				 else {
+					 String crtno = crt.getCrtNo();
+					 System.out.println(crtno);
+//					 String newCrt = String.valueOf(Integer.parseInt(crt.getCrtNo()) + 1);
+      			     String newCrt = String.valueOf(Integer.parseInt(crtno) + 1);
+					 System.out.println(newCrt);
+					 crt.setCrtNo(newCrt);
+					 crtIdMapper.update(crt);
+					 record.setHotelCode(newCrt);
+				 }
+				 try
+				 {
+					 bizHotlMapper.add(record);
+				 }
+				 catch (Exception e)
+				 {
+				 	/* 返回错误信息  预留开发*/
+					  HttpResult.error(e.getMessage());
+				 }
+                return 0;
+            }
+		System.out.println(record.getHotelCode());
+
 		return bizHotlMapper.update(record);
-	}
+
+       // return bizHotlMapper.add(record);
+
+    }
 
 	@Override
 	public int delete(BizHotl record) {
-		return bizHotlMapper.delete(record.getId());
+		System.out.println(record.getHotelCode());
+		return bizHotlMapper.delete(record.getHotelCode());
 	}
 
 	@Override
