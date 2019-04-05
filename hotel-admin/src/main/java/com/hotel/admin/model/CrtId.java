b71d98d059d219734bd7e228b772bd193416d8a1
ZengCailong <47208120+ZengCailong@users.noreply.github.com>
2019-04-05 17:46:28 +0800

    默认的

---
+++ hotel-admin/src/main/java/com/hotel/admin/model/CrtId.java
@@ -0,0 +1,35 @@
+package com.hotel.admin.model;
+
+/**
+ * ---------------------------
+ * 自增序列表 (CrtId)         
+ * ---------------------------
+ * 作者：  kitty-generator
+ * 时间：  2019-04-05 14:24:29
+ * 说明：  我是由代码生成器生生成的
+ * ---------------------------
+ */
+public class CrtId {
+
+	/** 当前可用编号 */
+	private String crtNo;
+	/** 编号类型 */
+	private String crtType;
+
+	public String getCrtNo() {
+		return crtNo;
+	}
+
+	public void setCrtNo(String crtNo) {
+		this.crtNo = crtNo;
+	}
+
+	public String getCrtType() {
+		return crtType;
+	}
+
+	public void setCrtType(String crtType) {
+		this.crtType = crtType;
+	}
+
+}
