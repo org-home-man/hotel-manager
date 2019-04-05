ab8405964007f3d25900f94bc0f731ce1202fa22
ZengCailong <47208120+ZengCailong@users.noreply.github.com>
2019-04-05 17:42:52 +0800

    默认的

---
+++ hotel-admin/src/main/java/com/hotel/admin/service/impl/CrtIdServiceImpl.java
@@ -0,0 +1,63 @@
+package com.hotel.admin.service.impl;
+
+import java.util.List;
+import com.hotel.admin.model.CrtId;
+import com.hotel.admin.service.CrtIdService;
+import com.hotel.admin.mapper.CrtIdMapper;
+import com.hotel.core.page.MybatisPageHelper;
+import com.hotel.core.page.PageRequest;
+import com.hotel.core.page.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * ---------------------------
+ * 自增序列表 (CrtIdServiceImpl)         
+ * ---------------------------
+ * 作者：  kitty-generator
+ * 时间：  2019-04-05 15:23:50
+ * 说明：  我是由代码生成器生生成的
+ * ---------------------------
+ */
+@Service
+public class CrtIdServiceImpl implements CrtIdService {
+
+	@Autowired
+	private CrtIdMapper crtIdMapper;
+
+	@Override
+	public int save(CrtId record) {
+		if(record.getCrtType() == null || record.getCrtType() == "0") {
+			return crtIdMapper.add(record);
+		}
+		return crtIdMapper.update(record);
+	}
+
+	@Override
+	public int delete(CrtId record) {
+		return crtIdMapper.delete(record.getCrtType());
+	}
+
+	@Override
+	public int delete(List<CrtId> records) {
+		for(CrtId record:records) {
+			delete(record);
+		}
+		return 1;
+	}
+
+	@Override
+	public CrtId findById(Long id) {
+		return null;
+	}
+	@Override
+	public CrtId findById(String id) {
+		return crtIdMapper.findById(id);
+	}
+
+	@Override
+	public PageResult findPage(PageRequest pageRequest) {
+		return MybatisPageHelper.findPage(pageRequest, crtIdMapper);
+	}
+	
+}
