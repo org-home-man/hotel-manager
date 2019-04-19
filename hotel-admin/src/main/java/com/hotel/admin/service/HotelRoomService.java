b469687a0327622c3b9d71acd772581fea78028d
lichengyu <47208120+ZengCailong@users.noreply.github.com>
2019-04-19 22:38:42 +0800

    默认的

---
+++ hotel-admin/src/main/java/com/hotel/admin/service/HotelRoomService.java
@@ -0,0 +1,24 @@
+package com.hotel.admin.service;
+
+import com.hotel.admin.model.BizRoom;
+import com.hotel.core.service.CurdService;
+
+/**
+ * ---------------------------
+ * 客房信息表 (BizRoomService)         
+ * ---------------------------
+ * 作者：  kitty-generator
+ * 时间：  2019-04-01 21:00:17
+ * 说明：  我是由代码生成器生生成的
+ * ---------------------------
+ */
+public interface HotelRoomService extends CurdService<BizRoom> {
+
+    /**
+     * 根据ID查询
+     * @param id
+     * @return
+     */
+    BizRoom  findById(String id);
+
+}
