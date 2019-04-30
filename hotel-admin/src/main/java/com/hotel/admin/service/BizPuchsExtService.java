759d870876cc564fe5ef0eb96fdf1eaaf66897dd
lichengyu <47208120+ZengCailong@users.noreply.github.com>
2019-04-30 15:45:07 +0800

    默认的

---
+++ hotel-admin/src/main/java/com/hotel/admin/service/BizPuchsExtService.java
@@ -0,0 +1,18 @@
+package com.hotel.admin.service;
+
+import com.hotel.admin.model.BizPuchsExt;
+import com.hotel.core.service.CurdService;
+
+/**
+ * ---------------------------
+ * 订单信息辅助表 (BizPuchsExtService)         
+ * ---------------------------
+ * 作者：  kitty-generator
+ * 时间：  2019-04-30 12:34:59
+ * 说明：  我是由代码生成器生生成的
+ * ---------------------------
+ */
+public interface BizPuchsExtService extends CurdService<BizPuchsExt> {
+    BizPuchsExt findById(String id);
+
+}
