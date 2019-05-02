4e3f60c125a39474ebab2f83a116ea6032364605
lichengyu <47208120+ZengCailong@users.noreply.github.com>
2019-05-02 13:52:27 +0800

    默认的

---
+++ hotel-admin/src/main/java/com/hotel/admin/service/impl/BizPuchsExtServiceImpl.java
@@ -0,0 +1,65 @@
+package com.hotel.admin.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hotel.core.page.MybatisPageHelper;
+import com.hotel.core.page.PageRequest;
+import com.hotel.core.page.PageResult;
+
+import com.hotel.admin.model.BizPuchsExt;
+import com.hotel.admin.mapper.BizPuchsExtMapper;
+import com.hotel.admin.service.BizPuchsExtService;
+
+/**
+ * ---------------------------
+ * 订单信息辅助表 (BizPuchsExtServiceImpl)         
+ * ---------------------------
+ * 作者：  kitty-generator
+ * 时间：  2019-04-30 12:34:59
+ * 说明：  我是由代码生成器生生成的
+ * ---------------------------
+ */
+@Service
+public class BizPuchsExtServiceImpl implements BizPuchsExtService {
+
+	@Autowired
+	private BizPuchsExtMapper bizPuchsExtMapper;
+
+	@Override
+	public int save(BizPuchsExt record) {
+		if(record.getRoomCode() == null || record.getRoomCode() == "0") {
+			return bizPuchsExtMapper.add(record);
+		}
+		return bizPuchsExtMapper.update(record);
+	}
+
+	@Override
+	public int delete(BizPuchsExt record) {
+		return bizPuchsExtMapper.delete(record.getOrderCode());
+	}
+	@Override
+	public BizPuchsExt findById(Long id) {
+		return null;
+	}
+	@Override
+	public int delete(List<BizPuchsExt> records) {
+		for(BizPuchsExt record:records) {
+			delete(record);
+		}
+		return 1;
+	}
+
+	@Override
+	public BizPuchsExt findById(String id) {
+		return bizPuchsExtMapper.findById(id);
+	}
+
+	@Override
+	public PageResult findPage(PageRequest pageRequest) {
+		return MybatisPageHelper.findPage(pageRequest, bizPuchsExtMapper);
+	}
+	
+}
