c8522f41abc4bd6b127e48131de1b79090132ba0
chenchao <466027149@qq.com>
2019-05-17 17:37:15 +0800

    全局异常拦截

---
+++ hotel-admin/src/main/java/com/hotel/admin/advice/GlobalControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.hotel.admin.advice;
+
+import com.hotel.core.exception.GlobalException;
+import com.hotel.core.http.HttpResult;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice("com.hotel.admin.controller")
+public class GlobalControllerAdvice {
+
+    @ResponseBody
+    @ExceptionHandler
+    public HttpResult globalExceptionHandler(GlobalException gblException){
+        return HttpResult.error(gblException.getCode(),gblException.getMsg());
+    }
+}
