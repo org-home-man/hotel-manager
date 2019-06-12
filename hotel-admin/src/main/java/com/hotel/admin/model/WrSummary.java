a6adbfd96e86e41fa71394c6d31fca6708d3a995
chenchao <466027149@qq.com>
2019-06-12 17:21:56 +0800

    notice

---
+++ hotel-admin/src/main/java/com/hotel/admin/model/WrSummary.java
@@ -0,0 +1,110 @@
+package com.hotel.admin.model;
+
+import com.hotel.common.entity.Entity;
+
+public class WrSummary extends Entity {
+    /**
+     * 
+     */
+    private String reportId;
+
+    /**
+     * 
+     */
+    private String reportTxt;
+
+    /**
+     * 
+     */
+    private String reportMonth;
+
+    /**
+     * 
+     */
+    private String reportSeq;
+
+    /**
+     * 
+     */
+    private String creatTime;
+
+    /**
+     * 
+     * @return report_id 
+     */
+    public String getReportId() {
+        return reportId;
+    }
+
+    /**
+     * 
+     * @param reportId 
+     */
+    public void setReportId(String reportId) {
+        this.reportId = reportId == null ? null : reportId.trim();
+    }
+
+    /**
+     * 
+     * @return report_txt 
+     */
+    public String getReportTxt() {
+        return reportTxt;
+    }
+
+    /**
+     * 
+     * @param reportTxt 
+     */
+    public void setReportTxt(String reportTxt) {
+        this.reportTxt = reportTxt == null ? null : reportTxt.trim();
+    }
+
+    /**
+     * 
+     * @return report_month 
+     */
+    public String getReportMonth() {
+        return reportMonth;
+    }
+
+    /**
+     * 
+     * @param reportMonth 
+     */
+    public void setReportMonth(String reportMonth) {
+        this.reportMonth = reportMonth == null ? null : reportMonth.trim();
+    }
+
+    /**
+     * 
+     * @return report_seq 
+     */
+    public String getReportSeq() {
+        return reportSeq;
+    }
+
+    /**
+     * 
+     * @param reportSeq 
+     */
+    public void setReportSeq(String reportSeq) {
+        this.reportSeq = reportSeq == null ? null : reportSeq.trim();
+    }
+
+    /**
+     * 
+     * @return creat_time 
+     */
+    public String getCreatTime() {
+        return creatTime;
+    }
+
+    /**
+     * 
+     * @param creatTime 
+     */
+    public void setCreatTime(String creatTime) {
+        this.creatTime = creatTime == null ? null : creatTime.trim();
+    }
+}
