8ce21d75922d036917d07ec074107e2775a2140c
zcl <15002808281@163.com>
2019-09-17 20:52:50 +0800

    需求变更

---
+++ hotel-admin/src/main/java/com/hotel/admin/qo/BizInvQo.java
@@ -0,0 +1,35 @@
+package com.hotel.admin.qo;
+
+public class BizInvQo {
+
+    private Integer inventory;
+
+    private String hotelCode;
+
+    private String invDate;
+
+
+    public Integer getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Integer inventory) {
+        this.inventory = inventory;
+    }
+
+    public String getHotelCode() {
+        return hotelCode;
+    }
+
+    public void setHotelCode(String hotelCode) {
+        this.hotelCode = hotelCode;
+    }
+
+    public String getInvDate() {
+        return invDate;
+    }
+
+    public void setInvDate(String invDate) {
+        this.invDate = invDate;
+    }
+}
