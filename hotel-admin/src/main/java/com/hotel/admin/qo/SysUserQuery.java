c5864e15a6b44f3dca858d8541fdbcbbca449ba2
lichengyu <47208120+ZengCailong@users.noreply.github.com>
2019-10-08 14:09:54 +0800

    默认的

--- hotel-admin/src/main/java/com/hotel/admin/qo/SysUserQuery.java
+++ hotel-admin/src/main/java/com/hotel/admin/qo/SysUserQuery.java
@@ -4,7 +4,27 @@ public class SysUserQuery {
 
     private String name;
 
+    private  String userRoles;
+
+    private  String org;
+
     private String id;
+
+    public String getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(String userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    public String getOrg() {
+        return org;
+    }
+
+    public void setOrg(String org) {
+        this.org = org;
+    }
 
     public String getName() {
         return name;
